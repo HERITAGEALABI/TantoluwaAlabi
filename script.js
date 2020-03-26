1fda982b75c77b740ecb1ed8b40896e79af0767a
ALABI TANTOLUWA HERITAGE <56360427+HERITAGEALABI@users.noreply.github.com>
2020-03-26 23:49:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var name = "Heritage";
+var courses = ['Maths', 'English', 'Biology', 'Eng 202'];
+
+if (courses.length % 2 === 0) {
+    for (var i = 0; i <= 200; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+    }
+}
+
+else {
+    for (var j = 0; j <= 200; j++) {
+        if (j % 2 !== 0) {
+            console.log(j)
+        }
+    }
+}
